80a9dd6a2a53e37f3a89301a036742290908a720
meriemalg <meriemmeriem19alg@gmail.com>
2024-04-07 15:27:59 +0200

    'added to allowed'

--- pro2/settings.py
+++ pro2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xt(tw)(0u%8^jzp*18$zotqd@p8t9_pqyug0)#o!nkvdmvh-tc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [ "real-estate-pfe.onrender.com"]
 
 
 # Application definition
