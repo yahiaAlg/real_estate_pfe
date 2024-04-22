04a0a5e0efb980adf722205e42bdab9777c52bf7
meriemalg <166303849+meriemalg@users.noreply.github.com>
2024-04-22 12:12:21 +0100

    Update2 admin.py

--- listings/admin.py
+++ listings/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Listing
 #from .models import Realtor
 class ListingAdmin(admin.ModelAdmin):
+  list_display = ['title','add_date']
   list_filter = ['add_date']
 # Register your models here.
 admin.site.register(Listing) ##pour lier model par site admin
