c330ff9103147ee5fa1e7731b8ff8dd05855c17d
meriemalg <166303849+meriemalg@users.noreply.github.com>
2024-04-22 12:19:25 +0100

    Update3 admin.py

--- listings/admin.py
+++ listings/admin.py
@@ -5,5 +5,5 @@ class ListingAdmin(admin.ModelAdmin):
   list_display = ['title','add_date']
   list_filter = ['add_date']
 # Register your models here.
-admin.site.register(Listing) ##pour lier model par site admin
+admin.site.register(Listing,ListingAdmin) ##pour lier model par site admin
 #admin.site.register(Realtor)
